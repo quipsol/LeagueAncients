930efddf03ed5601c4be6b612c83589e2a6be5f9
quipsol <245758987+quipsol@users.noreply.github.com>
2026-07-31 23:31:19 +0200

    Add 9th Kayle relic

--- LeagueAncientsCode/Core/Content/Ancients/Kayle.cs
+++ LeagueAncientsCode/Core/Content/Ancients/Kayle.cs
@@ -2,24 +2,17 @@
 using Godot;
 using LeagueAncients.Core.Content.Relics;
 using LeagueAncients.Core.Models;
-using MegaCrit.Sts2.Core.Context;
-using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Models.Relics;
-using MegaCrit.Sts2.Core.Runs;
 
 namespace LeagueAncients.Core.Content.Ancients;
 
 
 public class Kayle : LeagueAncientsAncientModel
 {
-     public override string? CustomMapIconPath => "res://LeagueAncients/images/placeholder/100_100/purple.png";
-     public override string? CustomMapIconOutlinePath => "res://LeagueAncients/images/placeholder/150_150/black.png";
-     public override string? CustomRunHistoryIconPath => "res://LeagueAncients/images/placeholder/100_100/white.png";
-     public override string? CustomRunHistoryIconOutlinePath => "res://LeagueAncients/images/placeholder/150_150/black.png";
-    
-    //public override string? CustomBackgroundScenePath => "res://SlayRuneterra/scenes/events/background_scenes/kayle.tscn";
-
-    public override string? CustomScenePath => "res://LeagueAncients/scenes/events/background_scenes/kayle.tscn";
+     public override string CustomMapIconPath => "res://LeagueAncients/images/placeholder/100_100/purple.png";
+     public override string CustomMapIconOutlinePath => "res://LeagueAncients/images/placeholder/150_150/black.png";
+     public override string CustomRunHistoryIconPath => "res://LeagueAncients/images/placeholder/100_100/white.png";
+     public override string CustomRunHistoryIconOutlinePath => "res://LeagueAncients/images/placeholder/150_150/black.png";
+    public override string CustomScenePath => "res://LeagueAncients/scenes/events/background_scenes/kayle.tscn";
 
     public override Color ButtonColor => new Color(0.05f, 0.06f, 0.12f, 0.8f);
     public override Color DialogueColor => new Color("3C1931");
@@ -34,15 +27,16 @@ public class Kayle : LeagueAncientsAncientModel
                             AncientOption<DivineHelmet>(),
                             AncientOption<DivineIdol>() 
                 ),
+                // Essence of an Angel, Ring of Carnage,
+                MakePool(
+                            AncientOption<RingOfCarnage>(),
+                            AncientOption<EssenceOfAnAngel>(),
+                            AncientOption<GuidingLight>()
+                ),
                 // Flail of Judgement, Sword of Judgement, Spear of Judgement
                 MakePool(
                             AncientOption<SpearOfJustice>(),
                             AncientOption<SwordOfJustice>(),
                             AncientOption<FlailOfJustice>()
-                ),
-                // Essence of an Angel, Ring of Carnage,
-                MakePool(
-                            AncientOption<RingOfCarnage>(),
-                            AncientOption<EssenceOfAnAngel>()
                 ));
 }
---
+++ LeagueAncientsCode/Core/Content/Relics/GuidingLight.cs
@@ -0,0 +1,31 @@
+﻿using BaseLib.Utils;
+using LeagueAncients.Core.Content.Cards;
+using LeagueAncients.Core.Models;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+
+namespace LeagueAncients.Core.Content.Relics;
+
+
+[Pool(typeof(EventRelicPool))]
+public class GuidingLight : LeagueAncientsRelicModel
+{
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+
+
+    public override bool TryModifyEnergyCostInCombat(CardModel card, decimal currentCost, out decimal modifiedCost)
+    {
+        if (card.Type is CardType.Attack)
+        {
+            modifiedCost =  currentCost - 1;
+            return true;
+        }
+        modifiedCost = currentCost;
+        return false;
+    }
+}
